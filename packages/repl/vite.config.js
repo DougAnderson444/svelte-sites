3d5440726ebfaddb310c4a23b31007c39b85d707
Doug A <douganderson444@gmail.com>
2022-09-15 15:08:38 -0300

    create vite.config file

---
+++ packages/repl/vite.config.js
@@ -0,0 +1,19 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+
+const config = {
+	plugins: [sveltekit()],
+	resolve: {
+		alias: {
+			'@sveltejs/repl': path.resolve('src/lib'),
+			'@sveltejs/site-kit': path.resolve('../site-kit/src/lib')
+		}
+	},
+	server: {
+		fs: {
+			strict: false
+		}
+	},
+	worker: {
+		format: 'es' //  default is 'iife' // https://vitejs.dev/config/#worker-format
+	}
+};
