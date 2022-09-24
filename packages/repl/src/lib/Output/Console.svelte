46715b1674f29a8001f27ea71c6a0dd8c11074b2
Doug A <douganderson444@gmail.com>
2022-09-24 11:41:28 -0300

    rename container, add css

--- packages/repl/src/lib/Output/Console.svelte
+++ packages/repl/src/lib/Output/Console.svelte
@@ -4,8 +4,18 @@
 	export let logs;
 </script>
 
-<div class="container">
+<div class="console-container">
 	{#each logs as log}
 		<ConsoleLine {log} />
 	{/each}
 </div>
+
+<style>
+	.console-container {
+		position: absolute;
+		background-color: white;
+		border: none;
+		width: 100%;
+		height: 100%;
+	}
+</style>
